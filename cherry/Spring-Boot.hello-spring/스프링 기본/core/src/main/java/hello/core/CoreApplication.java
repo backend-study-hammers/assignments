44e9507c067f10c7ee10370aa63238db749f872e
chaeri93 <chaeri93@ewhain.net>
2021-07-29 10:00:16 +0900

    Spring 기본 Day 3 code upload

--- cherry/Spring-Boot.hello-spring/스프링 기본/core/src/main/java/hello/core/CoreApplication.java
+++ cherry/Spring-Boot.hello-spring/스프링 기본/core/src/main/java/hello/core/CoreApplication.java
@@ -1,6 +1,6 @@
 package hello.core;
 
-import org.springframework.boot.SpringApplication;
+import  org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
